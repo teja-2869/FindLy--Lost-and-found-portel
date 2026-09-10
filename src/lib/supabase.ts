a242f9c78b0596c6196af2709aa71ef5ec6ec9bd
sreenivasa <ailasreenivasateja@gmail.com>
2026-09-10 15:15:19 +0530

    new initial commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://oodfplfuyntblxnothln.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vZGZwbGZ1eW50Ymx4bm90aGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNjA1MTcsImV4cCI6MjA5NjczNjUxN30.A9vKWoJcCWMEtX1jLEe16SFaNcbAeFjilKJs89cklbU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://bcfgryrpahbjxhnduane.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_ob_ExR5R8kxQEUTFgBah6Q_amiQm0HF';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
